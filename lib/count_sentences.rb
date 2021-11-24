082b349f26e97ad2a088682287f20acc6364c710
Luis Escobar <lae311@stern.nyu.edu>
2021-11-24 14:26:42 -0500

    commit tree f7a97a38e45c3b3c85b9359fc30c59ad7ddbeca9

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
+# I dont even have to initialize with a variable to use a method
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
   end
+
 end
+
+complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+puts complex_string.count_sentences
